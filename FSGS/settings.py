d612f43ce06cd94cc3f06355ac4fbd2d8861c898
raspring <robert_springett@yahoo.com>
2025-12-09 22:09:52 +0800

    Prep for Railway deployment

--- FSGS/settings.py
+++ FSGS/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FSGS.wsgi --log-file -
