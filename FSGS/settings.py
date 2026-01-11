c4eafdd41330fc62408ebfbc770f339cb5af3915
raspring <robert_springett@yahoo.com>
2026-01-11 11:26:22 +0800

    Fix DATABASES to use dj_database_url with Railway Postgres

--- FSGS/settings.py
+++ FSGS/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")  # Railway Postgres
     )
 }
 
